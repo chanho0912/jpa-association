c859c93b7156629db5d110782959d366555bf282
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-11-01 00:22:37 +0900

    refactor: 사용 안하는 코드 제거

--- src/main/java/persistence/entity/EntityLoader.java
+++ src/main/java/persistence/entity/EntityLoader.java
@@ -1,14 +1,10 @@
 package persistence.entity;
 
-import jdbc.EagerFetchRowMapper;
 import jdbc.JdbcTemplate;
 import jdbc.RowMapperFactory;
-import persistence.sql.definition.EntityTableMapper;
 import persistence.sql.definition.TableAssociationDefinition;
 import persistence.sql.definition.TableDefinition;
 import persistence.sql.dml.query.SelectQueryBuilder;
-
-import java.util.Collection;
 
 public class EntityLoader {
     private final JdbcTemplate jdbcTemplate;
@@ -32,19 +28,5 @@ public class EntityLoader {
         );
 
         return entityClass.cast(queried);
-    }
-
-    public <T> Collection<T> loadLazyCollection(Class<T> targetClass, EntityTableMapper ownerTableMapper) {
-        final SelectQueryBuilder queryBuilder = new SelectQueryBuilder(targetClass);
-        final String joinColumnName = ownerTableMapper.getJoinColumnName(targetClass);
-        final Object value = ownerTableMapper.getValue(joinColumnName);
-
-        final String query = queryBuilder
-                .where(joinColumnName, value.toString())
-                .build();
-
-        return jdbcTemplate.query(query,
-                new EagerFetchRowMapper<>(targetClass)
-        );
     }
 }
