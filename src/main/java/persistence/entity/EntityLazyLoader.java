62d7238db1d0055fd3ad5ee24c68c3d273bcdc14
chanho0912-kim <chanho0912.kim@linecorp.com>
2024-11-01 00:28:16 +0900

    refactor: interface에 public 제거

--- src/main/java/persistence/entity/EntityLazyLoader.java
+++ src/main/java/persistence/entity/EntityLazyLoader.java
@@ -5,5 +5,5 @@ import persistence.sql.definition.EntityTableMapper;
 import java.util.Collection;
 
 public interface EntityLazyLoader {
-    public <T> Collection<T> loadLazyCollection(Class<T> targetClass, EntityTableMapper ownerTableMapper);
+    <T> Collection<T> loadLazyCollection(Class<T> targetClass, EntityTableMapper ownerTableMapper);
 }
